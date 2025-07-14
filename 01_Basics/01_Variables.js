c41c90874ba950ff6f8e7e922c0793f1586ebb94
Anush Shetty <anushshetty135@gmail.com>
2025-07-14 04:42:59 +0000

    commit

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,21 @@
+const accountId = "135689"
+let accountEmail = "anushshetty135@gmail.com"
+var accountPassword = "07656"
+accountCity = "Mumbai"
+let accountState
+
+console.log(accountId)
+
+accountEmail= "anushshetty79@gmail.com"
+accountPassword = "45656"
+accountCity = "Pune"
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountEmail,accountPassword,accountCity,accountState])
+
+
+
