e4945b0f4358c594dfd0d80e13d1690af6c05e92
Anush Shetty <anushshetty135@gmail.com>
2025-07-26 05:31:41 +0000

    Changes

--- 03_Basics/02_Scopes.js
+++ 03_Basics/02_Scopes.js
@@ -15,7 +15,7 @@ if (true) {
 
 
 function one(){
-    const username = "hitesh"
+    const username = "Anush"
 
     function two(){
         const website = "youtube"
@@ -30,8 +30,8 @@ function one(){
 // one()
 
 if (true) {
-    const username = "hitesh"
-    if (username === "hitesh") {
+    const username = "Anush"
+    if (username === "Anush") {
         const website = " youtube"
         // console.log(username + website);
     }
