98f8b66a004e31cce9f4d31a6d0e35370694e6a5
Anush Shetty <anushshetty135@gmail.com>
2025-08-20 01:23:48 +0000

    Learning about prototype in JS

---
+++ 09_Classes_and_OOPs/Prototype.js
@@ -0,0 +1,67 @@
+// let myName = "Anush     "
+// let mychannel = "chai     "
+
+// console.log(myName.trueLength);
+
+
+let myHeros = ["thor", "spiderman"]
+
+
+let heroPower = {
+    thor: "hammer",
+    spiderman: "sling",
+
+    getSpiderPower: function(){
+        console.log(`Spidy power is ${this.spiderman}`);
+    }
+}
+
+Object.prototype.anush = function(){
+    console.log(`Anush is present in all objects`);
+}
+
+Array.prototype.heyanush = function(){
+    console.log(`Anush says hello`);
+}
+
+// heroPower.anush()
+// myHeros.anush()
+// myHeros.heyanush()
+// heroPower.heyanush()
+
+// inheritance
+
+const User = {
+    name: "chai",
+    email: "chai@google.com"
+}
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment: 'JS assignment',
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+
+Teacher.__proto__ = User
+
+// modern syntax
+Object.setPrototypeOf(TeachingSupport, Teacher)
+
+let anotherUsername = "ChaiAurCode     "
+
+String.prototype.trueLength = function(){
+    console.log(`${this}`);
+    console.log(`True length is: ${this.trim().length}`);
+}
+
+anotherUsername.trueLength()
+"hitesh".trueLength()
+"iceTea".trueLength()
